ff9d05e09593ba310b39d866b3c864eb0347e853
fsmosca <fsmosca@users.noreply.github.com>
2023-09-30 14:50:07 +0800

    Add rate limit for up to 5 requests per minute

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from typing import Optional, List, Dict
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
 import pandas as pd
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
 
 ZONES = ['africa', 'americas', 'asia', 'europe']
+RATE_LIMITS = '5/minute'  # 5 requests per minute
 
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
 @app.get("/worldcup/2019/championship")
-async def wordcup_2019_championship(team: Optional[str] = None) -> List[Dict]:
+@limiter.limit(RATE_LIMITS)
+async def wordcup_2019_championship(request: Request, team: Optional[str] = None) -> List[Dict]:
     """## Gets FIBA world cup 2019 championship game results.
 
     If team is not specified, all results will be returned.
@@ -45,7 +55,8 @@ async def wordcup_2019_championship(team: Optional[str] = None) -> List[Dict]:
 
 
 @app.get("/worldcup/2023/championship")
-async def wordcup_2023_championship(team: Optional[str] = None) -> List[Dict]:
+@limiter.limit(RATE_LIMITS)
+async def wordcup_2023_championship(request: Request, team: Optional[str] = None) -> List[Dict]:
     """## Gets FIBA world cup 2023 championship game results.
 
     If team is not specified, all results will be returned.
@@ -78,7 +89,9 @@ async def wordcup_2023_championship(team: Optional[str] = None) -> List[Dict]:
 
 
 @app.get("/worldcup/2023/qualifier")
+@limiter.limit(RATE_LIMITS)
 async def wordcup_2023_qualifier(
+    request: Request,
     team: Optional[str] = None,
     zone: Optional[str] = None
 ) -> List[Dict]:
